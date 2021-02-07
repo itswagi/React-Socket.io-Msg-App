f2682969a2260b15fc658250f6e3570a054229c8
itswagi <43064115+itswagi@users.noreply.github.com>
2021-02-07 20:31:55 +0500

    Added Socket.io code

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 require('dotenv').config()
 
 const PORT = 4000
 const app = express()
+
+const server = createServer(app)
+const io = new Server(server)
+io.on('connection', socket => {
+  console.log('user connected')
+})
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
@@ -13,6 +21,6 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
